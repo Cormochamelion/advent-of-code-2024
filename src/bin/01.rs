acb5375a757b2fd049b4c9bf81f135b2feede6c5
cormochamelion <cormocodran1.5@gmail.com>
2024-12-01 21:17:33 +0100

    Day 1: Finish part 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,7 +1,46 @@
 advent_of_code::solution!(1);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+fn input_to_pair_vec(input: &str) -> Vec<Vec<u32>> {
+    let mut left_vec = Vec::new();
+    let mut right_vec = Vec::new();
+
+    for line in input.lines() {
+        let value_vec = line
+            .split_whitespace()
+            .map(|value| value.parse::<u32>().expect("Couldn't parse string to int."))
+            .collect::<Vec<u32>>();
+
+        let [left, right] = value_vec.as_slice() else {
+            panic!("Not exactly two values to unpack.")
+        };
+
+        left_vec.push(left.to_owned());
+        right_vec.push(right.to_owned());
+    }
+
+    vec![left_vec, right_vec]
+}
+
+pub fn part_one(input: &str) -> Option<i32> {
+    let mut pair_vec = input_to_pair_vec(input);
+
+    for vec in pair_vec.iter_mut() {
+        vec.sort()
+    }
+
+    let [left_vec, right_vec] = pair_vec.as_slice() else {
+        panic!("Can't unpack values.")
+    };
+
+    let mut total_distance: i32 = 0;
+
+    for pair in left_vec.iter().zip(right_vec.iter()) {
+        let (left, right) = pair;
+        let diff: i32 = *left as i32 - *right as i32;
+        total_distance += diff.abs();
+    }
+
+    Some(total_distance)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
