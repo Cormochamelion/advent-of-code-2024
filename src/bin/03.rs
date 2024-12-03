f71f22a3e7471bd5c57481f98affd6b7c6cfa190
cormochamelion <cormocodran1.5@gmail.com>
2024-12-03 10:54:11 +0100

    Day 3: Slightly simplify part 1
      I added some things I thought might be useful for part 2,
      turns out they weren't.

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -3,7 +3,7 @@ advent_of_code::solution!(3);
 use regex::Regex;
 
 pub fn part_one(input: &str) -> Option<i32> {
-    let mult_re: Regex = Regex::new("(mul)\\(([0-9]+),([0-9]+)\\)").unwrap();
+    let mult_re: Regex = Regex::new("mul\\(([0-9]+),([0-9]+)\\)").unwrap();
 
     let match_groups = mult_re
         .captures_iter(input)
@@ -11,16 +11,11 @@ pub fn part_one(input: &str) -> Option<i32> {
 
     let mut total_operations_sum: i32 = 0;
 
-    for (_, [op, arg1_str, arg2_str]) in match_groups {
+    for (_, [arg1_str, arg2_str]) in match_groups {
         let arg1 = arg1_str.parse::<i32>().unwrap();
         let arg2 = arg2_str.parse::<i32>().unwrap();
 
-        let op_res = match op {
-            "mul" => arg1 * arg2,
-            _ => panic!("Unknown operartion {}", op),
-        };
-
-        total_operations_sum += op_res;
+        total_operations_sum += arg1 * arg2;
     }
 
     Some(total_operations_sum)
