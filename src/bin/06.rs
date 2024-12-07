9fe18c841014d2d30d29d871d184ba9ef9c20555
cormochamelion <cormocodran1.5@gmail.com>
2024-12-07 03:36:49 +0100

    Day 6: Finish part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -28,6 +28,8 @@ struct Guard<'a> {
     curr_pos: (usize, usize),
     curr_dir: (i32, i32),
     visited_positions: HashSet<(usize, usize)>,
+    known_dir_changes: HashSet<(usize, usize, i32, i32)>,
+    looping_boxes: HashSet<(usize, usize)>,
     map: &'a Vec<Vec<char>>,
     dimensions: (usize, usize),
     directions_iter: IterType,
@@ -55,6 +57,80 @@ impl Guard<'_> {
 
         return true;
     }
+
+    fn count_new_boxes(&mut self) -> u32 {
+        loop {
+            let new_pos = (
+                (self.curr_pos.0 as i32 + self.curr_dir.0) as usize,
+                (self.curr_pos.1 as i32 + self.curr_dir.1) as usize,
+            );
+
+            if !(new_pos.0 < self.dimensions.0 && new_pos.1 < self.dimensions.1) {
+                break;
+            }
+
+            if self.map[new_pos.0][new_pos.1] == '#' {
+                // A box: Turn 90° right and log the position at which the box
+                // was encountered.
+                self.known_dir_changes.insert((
+                    self.curr_pos.0,
+                    self.curr_pos.1,
+                    self.curr_dir.0,
+                    self.curr_dir.1,
+                ));
+
+                self.curr_dir = self.directions_iter.next().unwrap();
+            } else {
+                if !self.looping_boxes.contains(&new_pos)
+                    && !self.visited_positions.contains(&new_pos)
+                    && self.new_box_loops(new_pos)
+                {
+                    self.looping_boxes.insert(new_pos);
+                }
+
+                self.visited_positions.insert(new_pos);
+                self.curr_pos = new_pos
+            }
+        }
+
+        return self.looping_boxes.len() as u32;
+    }
+
+    fn new_box_loops(&mut self, potential_box_pos: (usize, usize)) -> bool {
+        // Use function specific variables, the objects fields shouldn't
+        // change. Seems this is some shitty design.
+        let mut curr_pos = self.curr_pos.clone();
+        let mut directions_iter = self.directions_iter.clone();
+        let mut known_dir_changes = self.known_dir_changes.clone();
+
+        known_dir_changes.insert((curr_pos.0, curr_pos.1, self.curr_dir.0, self.curr_dir.1));
+
+        // A potential box: Turn 90° right.
+        let mut curr_dir = directions_iter.next().unwrap();
+
+        loop {
+            let new_pos = (
+                (curr_pos.0 as i32 + curr_dir.0) as usize,
+                (curr_pos.1 as i32 + curr_dir.1) as usize,
+            );
+
+            if !(new_pos.0 < self.dimensions.0 && new_pos.1 < self.dimensions.1) {
+                return false;
+            }
+
+            if self.map[new_pos.0][new_pos.1] == '#' || new_pos == potential_box_pos {
+                if !known_dir_changes.insert((curr_pos.0, curr_pos.1, curr_dir.0, curr_dir.1)) {
+                    return true;
+                }
+
+                // A box: Turn 90° right and log the position at which the box
+                // was encountered.
+                curr_dir = directions_iter.next().unwrap();
+            } else {
+                curr_pos = new_pos;
+            }
+        }
+    }
 }
 
 fn build_guard<'a>(init_pos: (usize, usize), map: &'a Vec<Vec<char>>) -> Guard {
@@ -67,6 +143,8 @@ fn build_guard<'a>(init_pos: (usize, usize), map: &'a Vec<Vec<char>>) -> Guard {
         curr_pos: init_pos,
         curr_dir: dir_iter.next().unwrap(),
         visited_positions: visited_set,
+        known_dir_changes: HashSet::new(),
+        looping_boxes: HashSet::new(),
         map: &map,
         dimensions: dimensions,
         directions_iter: dir_iter,
@@ -89,7 +167,11 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (init_pos, map) = parse_input(input);
+
+    let mut guard = build_guard(init_pos, &map);
+
+    Some(guard.count_new_boxes())
 }
 
 #[cfg(test)]
