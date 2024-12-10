1e64b7d8a6df1c4f812929195f95c8a2ab4e2a6e
cormochamelion <cormocodran1.5@gmail.com>
2024-12-10 16:39:31 +0100

    Day 10: Finish part 2
      Funnily this is the solution I went with for part 1 originally. ^^

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -84,8 +84,59 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(trail_head_score)
 }
 
+fn count_trails_from_trailhead(map: &Vec<Vec<u32>>, trail_head_pos: Vec<usize>) -> u32 {
+    let mut trail_head_score = 0;
+    let dims = vec![map.len(), map[0].len()];
+    let mut new_pos: Vec<usize>;
+    let mut new_height: u32;
+
+    let curr_height = map[trail_head_pos[0]][trail_head_pos[1]];
+
+    if curr_height == 9 {
+        return 1;
+    }
+
+    for dir in DIRECTIONS.iter().map(|dir| Vec::from(dir)) {
+        new_pos = trail_head_pos
+            .iter()
+            .zip(dir)
+            .map(|(coord, diff)| (*coord as i32 + diff) as usize)
+            .collect();
+
+        if new_pos[0] >= dims[0] || new_pos[1] >= dims[1] {
+            continue;
+        }
+
+        new_height = map[new_pos[0]][new_pos[1]];
+
+        if new_height.checked_sub(curr_height).unwrap_or(0) != 1 {
+            continue;
+        }
+
+        trail_head_score += count_trails_from_trailhead(map, new_pos);
+    }
+
+    trail_head_score
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    // Start from trail head (0).
+    // Look around for trail head + 1.
+    // Start from there.
+    // If trail head is 9 -> return 1
+    let topo_map = parse_input(input);
+    let mut trail_head_score = 0;
+
+    for i in 0..topo_map.len() {
+        for j in 0..topo_map[i].len() {
+            if topo_map[i][j] == 0 {
+                let score = count_trails_from_trailhead(&topo_map, vec![i, j]);
+                trail_head_score += score;
+            }
+        }
+    }
+
+    Some(trail_head_score)
 }
 
 #[cfg(test)]
@@ -101,6 +152,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(81));
     }
 }
