24d03215cd6bd3663e9243b298eb9033bea6b015
cormochamelion <cormocodran1.5@gmail.com>
2024-12-12 01:19:34 +0100

    Day 11: Finish part 2
      Admittedly after spoiling myself with this post:
      <https://www.reddit.com/r/adventofcode/comments/1hbo9m6/2024_day_11_today_i_learnt_humility/>
      by /u/TiCoinCoin.

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,6 +1,6 @@
 advent_of_code::solution!(11);
 
-use std::collections::VecDeque;
+use std::collections::{HashMap, VecDeque};
 
 fn parse_input(input: &str) -> VecDeque<u64> {
     input
@@ -58,8 +58,75 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(stones.len() as u32)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+fn parse_input_compressed(input: &str) -> HashMap<u64, u64> {
+    let mut input_map = HashMap::new();
+
+    for stone in input.split_whitespace().map(|x| x.parse::<u64>().unwrap()) {
+        match input_map.get_mut(&stone) {
+            Some(entry) => {
+                *entry += 1;
+            }
+            None => {
+                input_map.insert(stone, 1);
+            }
+        }
+    }
+
+    input_map
+}
+
+fn transmute_stone(stone: &u64) -> Vec<u64> {
+    let mut n_digits = 0;
+
+    if stone > &0u64 {
+        n_digits = stone.ilog10() + 1;
+    }
+
+    if stone == &0u64 {
+        return vec![1];
+    } else if n_digits.rem_euclid(2) == 0 {
+        let downshift = 10u64.pow(n_digits / 2);
+        let left_digits = stone / downshift;
+        let right_digits = stone - (left_digits * downshift);
+
+        return vec![left_digits, right_digits];
+    } else {
+        return vec![stone * 2024];
+    }
+}
+
+fn blink_compressed(stones: &mut HashMap<u64, u64>) {
+    let mut next_stones = HashMap::new();
+    let mut stone_products: Vec<u64>;
+
+    for (stone_val, count) in stones.iter() {
+        stone_products = transmute_stone(stone_val);
+
+        for product in stone_products.drain(..) {
+            match next_stones.get_mut(&product) {
+                Some(entry) => {
+                    *entry += count;
+                }
+                None => {
+                    next_stones.insert(product, *count);
+                }
+            }
+        }
+    }
+
+    *stones = next_stones;
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut stones = parse_input_compressed(input);
+
+    let n_iter = 75;
+
+    for _ in 0..n_iter {
+        blink_compressed(&mut stones);
+    }
+
+    Some(stones.iter().map(|(_, count)| count).sum())
 }
 
 #[cfg(test)]
