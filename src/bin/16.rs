efe931a3127efa3e5f0fc3b72c593257bd577ba5
cormochamelion <cormocodran1.5@gmail.com>
2024-12-16 11:00:29 +0100

    Day 16: Add part 1 test output

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -16,7 +16,7 @@ mod tests {
     fn test_part_one() {
         let result =
             part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11048));
     }
 
     #[test]
